775b62aa364f15e0366145315031e975698d8106
Adrianmo1998 <67030683+Adrianmo1998@users.noreply.github.com>
2020-07-10 23:55:45 -0500

    Pruebas unitarias
    Pruebas del controlador clientes

--- tests/Feature/Http/Controller/ClientesControllerTest.php
+++ tests/Feature/Http/Controller/ClientesControllerTest.php
@@ -6,8 +6,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+
 class ClientesControllerTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
     /**
      * A basic feature test example.
      *
@@ -20,53 +22,64 @@ class ClientesControllerTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function register_creates_and_authenticates_a_client()
+    public function test_register_creates_and_authenticates_a_client()
     {
-        $Nombre = $this->faker->Nombre;
-        $Apellido_Paterno = $this->faker->Apellido_Paterno;
-        $Apellido_Materno = $this->faker->Apellido_Materno;
-        $Direccion = $this->faker->Direccion;
-        $response = $this->post('register', [
-            'Nombre' => $Nombre,
-            'Apellido_Paterno' => $Apellido_Paterno,
-            'Apellido_Materno' => $Apellido_Materno,
-            'Direccion' => $Direccion,
+        $name = $this->faker->name;
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $address = $this->faker->address;
+        $response = $this->post('clientes', [
+            'Nombre' => $name,
+            'Apellido_Paterno' => $firstName,
+            'Apellido_Materno' => $lastName,
+            'Direccion' => $address
         ]);
-        $response->assertRedirect(route('clientes'));
         $this->assertDatabaseHas('clientes',[
-            'Nombre' => $Nombre,
-            'Apellido_Paterno' => $Apellido_Paterno,
-            'Apellido_Materno' => $Apellido_Materno,
-            'Direccion' => $Direccion,
+            'Nombre' => $name,
+            'Apellido_Paterno' => $firstName,
+            'Apellido_Materno' => $lastName,
+            'Direccion' => $address
         ]);
         }
-    
 
-    public function Edit_client()
-    {
-        $Nombre = $this->faker->Nombre;
-        $Apellido_Paterno = $this->faker->Apellido_Paterno;
-        $Apellido_Materno = $this->faker->Apellido_Materno;
-        $Direccion = $this->faker->Direccion;
-        $response = $this->post('edit', [
-            'Nombre' => $Nombre,
-            'Apellido_Paterno' => $Apellido_Paterno,
-            'Apellido_Materno' => $Apellido_Materno,
-            'Direccion' => $Direccion,
-        ]);
-        $response->assertRedirect(route('clientes'));
-        $this->assertDatabaseHas('clientes',[
-            'Nombre' => $Nombre,
-            'Apellido_Paterno' => $Apellido_Paterno,
-            'Apellido_Materno' => $Apellido_Materno,
-            'Direccion' => $Direccion,
-        ]);
-    }
-        
-        public function destroy_client()
+        public function test_get_client()
         {
-            $response = $this->post('delete/product', [] );
-    
-            $response->assertStatus(404);
-        }
+            $name = $this->faker->name;
+            $firstName = $this->faker->firstName;
+            $lastName = $this->faker->lastName;
+            $address = $this->faker->address;
+            $response = $this->put('clientes/.1', [
+                'Nombre' => $name,
+                'Apellido_Paterno' => $firstName,
+                'Apellido_Materno' => $lastName,
+                'Direccion' => $address
+            ]);
+            $this->assertDatabaseHas('clientes',[
+                'Nombre' => $name,
+                'Apellido_Paterno' => $firstName,
+                'Apellido_Materno' => $lastName,
+                'Direccion' => $address
+            ]);
+            }
+
+
+            public function test_delete_client()
+            {
+                $name = $this->faker->name;
+                $firstName = $this->faker->firstName;
+                $lastName = $this->faker->lastName;
+                $address = $this->faker->address;
+                $response = $this->delete('clientes/.1', [
+                    'Nombre' => $name,
+                    'Apellido_Paterno' => $firstName,
+                    'Apellido_Materno' => $lastName,
+                    'Direccion' => $address
+                ]);
+                $this->assertDatabaseHas('clientes',[
+                    'Nombre' => $name,
+                    'Apellido_Paterno' => $firstName,
+                    'Apellido_Materno' => $lastName,
+                    'Direccion' => $address
+                ]);
+                }
 }
